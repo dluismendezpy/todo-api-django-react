c15c733134903af96b4ffed15bd8d8c87611e60c
Luis Mendez <dluismendezdj@gmail.com>
2022-03-19 13:11:46 -0400

    Create Task model

--- api/models.py
+++ api/models.py
@@ -1,3 +1,22 @@
+# Django
 from django.db import models
 
-# Create your models here.
+
+class Task(models.Model):
+    """Model for all tasks"""
+    title = models.CharField(
+        max_length=200,
+        null=False,
+        blank=False,
+    )
+    completed = models.BooleanField(
+        default=False,
+        null=True,
+        blank=True,
+    )
+    created_at = models.DateTimeField(
+        auto_now_add=True
+    )
+
+    def __str__(self):
+        return self.title
