02e6090c63975b73d7d624705e5a4fcf95af10d1
Luis Mendez <dluismendezdj@gmail.com>
2022-03-19 13:13:21 -0400

    Resgister Task model

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,27 @@
+# Django
 from django.contrib import admin
 
-# Register your models here.
+# Own
+from .models import Task
+
+
+@admin.action(description='Mark selected todo as completed')
+def make_todos_completed(modeladmin, request, queryset):
+    queryset.update(completed=True)
+
+
+@admin.action(description='Mark selected todo as not completed')
+def make_todos_not_completed(modeladmin, request, queryset):
+    queryset.update(completed=False)
+
+
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    fields = ("title", "completed", "created_at")
+    list_display = ("title", "completed")
+    list_filter = ("completed", "created_at")
+    list_display_links = ("title",)
+    readonly_fields = ("created_at",)
+    ordering = ('-created_at',)
+    actions = (make_todos_completed, make_todos_not_completed)
+
