847c2331b6d1a9e0df2f320d770975df2cb8e16e
Luis Mendez <dluismendezdj@gmail.com>
2022-03-21 12:04:36 -0400

    Make react app

--- todo-frontend/src/App.js
+++ todo-frontend/src/App.js
@@ -1,25 +1,225 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      todoList: [],
+      activeItem: {
+        id: null,
+        title: "",
+        completed: false,
+      },
+      editing: false,
+    };
+    this.fetchTasks = this.fetchTasks.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.getCookie = this.getCookie.bind(this);
+
+    this.startEdit = this.startEdit.bind(this);
+    this.deleteItem = this.deleteItem.bind(this);
+    this.strikeUnstrike = this.strikeUnstrike.bind(this);
+  }
+
+  getCookie(name) {
+    let cookieValue = null;
+    if (document.cookie && document.cookie !== "") {
+      let cookies = document.cookie.split(";");
+      for (let i = 0; i < cookies.length; i++) {
+        let cookie = cookies[i].trim();
+        // Does this cookie string begin with the name we want?
+        if (cookie.substring(0, name.length + 1) === name + "=") {
+          cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+          break;
+        }
+      }
+    }
+    return cookieValue;
+  }
+
+  componentWillMount() {
+    this.fetchTasks();
+  }
+
+  fetchTasks() {
+    console.log("Fetching...");
+
+    fetch("http://127.0.0.1:8000/api/task-list/")
+      .then((response) => response.json())
+      .then((data) =>
+        this.setState({
+          todoList: data,
+        })
+      );
+  }
+
+  handleChange(e) {
+    let name = e.target.name;
+    let value = e.target.value;
+    console.log("Name:", name);
+    console.log("Value:", value);
+
+    this.setState({
+      activeItem: {
+        ...this.state.activeItem,
+        title: value,
+      },
+    });
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    console.log("ITEM:", this.state.activeItem);
+
+    let csrfToken = this.getCookie("csrftoken");
+
+    let url = "http://127.0.0.1:8000/api/task-create/";
+
+    if (this.state.editing === true) {
+      url = `http://127.0.0.1:8000/api/task-update/${this.state.activeItem.id}/`;
+      this.setState({
+        editing: false,
+      });
+    }
+
+    fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-type": "application/json",
+        "X-CSRFToken": csrfToken,
+      },
+      body: JSON.stringify(this.state.activeItem),
+    })
+      .then((response) => {
+        this.fetchTasks();
+        this.setState({
+          activeItem: {
+            id: null,
+            title: "",
+            completed: false,
+          },
+        });
+      })
+      .catch(function (error) {
+        console.log("ERROR:", error);
+      });
+  }
+
+  startEdit(task) {
+    this.setState({
+      activeItem: task,
+      editing: true,
+    });
+  }
+
+  deleteItem(task) {
+    let csrfToken = this.getCookie("csrftoken");
+
+    fetch(`http://127.0.0.1:8000/api/task-delete/${task.id}/`, {
+      method: "DELETE",
+      headers: {
+        "Content-type": "application/json",
+        "X-CSRFToken": csrfToken,
+      },
+    }).then((response) => {
+      this.fetchTasks();
+    });
+  }
+
+  strikeUnstrike(task) {
+    task.completed = !task.completed;
+    let csrfToken = this.getCookie("csrftoken");
+    let url = `http://127.0.0.1:8000/api/task-update/${task.id}/`;
+
+    fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-type": "application/json",
+        "X-CSRFToken": csrfToken,
+      },
+      body: JSON.stringify({ completed: task.completed, title: task.title }),
+    }).then(() => {
+      this.fetchTasks();
+    });
+
+    console.log("TASK:", task.completed);
+  }
+
+  render() {
+    let tasks = this.state.todoList;
+    let self = this;
+    return (
+      <div className="container">
+        <div className="task-container">
+          <div className="form-wrapper">
+            <form onSubmit={this.handleSubmit} id="form">
+              <div className="flex-wrapper">
+                <div style={{ flex: 6 }}>
+                  <input
+                    onChange={this.handleChange}
+                    className="form-control"
+                    id="title"
+                    value={this.state.activeItem.title}
+                    type="text"
+                    name="title"
+                    placeholder="Add todo..."
+                  />
+                </div>
+
+                <div style={{ flex: 1 }}>
+                  <input
+                    id="submit"
+                    className="btn btn-warning"
+                    type="submit"
+                    name="Add"
+                  />
+                </div>
+              </div>
+            </form>
+          </div>
+
+          <div id="list-wrapper">
+            {tasks.map(function (task, index) {
+              return (
+                <div key={index} className="task-wrapper flex-wrapper">
+                  <div
+                    onClick={() => self.strikeUnstrike(task)}
+                    style={{ flex: 7 }}
+                  >
+                    {task.completed === false ? (
+                      <span>{task.title}</span>
+                    ) : (
+                      <strike>{task.title}</strike>
+                    )}
+                  </div>
+
+                  <div style={{ flex: 0.5 }}>
+                    <button
+                      onClick={() => self.startEdit(task)}
+                      className="btn btn-sm btn-outline-primary"
+                    >
+                      Edit
+                    </button>
+                  </div>
+
+                  <div style={{ flex: 0.5 }}>
+                    <button
+                      onClick={() => self.deleteItem(task)}
+                      className="btn btn-sm btn-outline-danger"
+                    >
+                      Remove
+                    </button>
+                  </div>
+                </div>
+              );
+            })}
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
