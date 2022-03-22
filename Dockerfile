c2ed183c649f189b7c3f3616fe1b4779d11ebabe
Luis Mendez <dluismendezdj@gmail.com>
2022-03-22 14:17:05 -0400

    Add env variables

--- Dockerfile
+++ Dockerfile
@@ -3,6 +3,8 @@
 FROM python:3
 ENV PYTHONDONTWRITEBYTECODE=1
 ENV PYTHONUNBUFFERED=1
+ENV DEBUG=1
+ENV SECRET_KEY=django-insecure-jh!y^h#52+3l^+fm@(fph+a3kks-%y9i1&$k4_zas*imsvd79u
 WORKDIR /django
 COPY requirements.txt .
 RUN pip install -r requirements.txt
--- todo_api/settings.py
+++ todo_api/settings.py
@@ -11,7 +11,10 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import environ
 import os
+
+env = environ.Env(DEBUG=(bool, False))
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,12 +24,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-jh!y^h#52+3l^+fm@(fph+a3kks-%y9i1&$k4_zas*imsvd79u'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env('DEBUG')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
